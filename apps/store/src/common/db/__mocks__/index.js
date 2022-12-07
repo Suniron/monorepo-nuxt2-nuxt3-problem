d58f9f7e4077f9c097b5cfd5783a0db2865d2a48
Etienne BLANC <etienne@x-rator.com>
2022-12-07 21:48:49 +0100

    fix ci

--- apps/store/src/common/db/__mocks__/index.js
+++ apps/store/src/common/db/__mocks__/index.js
@@ -1,4 +1,4 @@
 // @ts-check
 export const queryDB = jest.fn()
 
-export const knex = jest.createMockFromModule('knex') // TODO useless ?
+// export const knex = jest.createMockFromModule('knex') // TODO useless ?
