87a872074829791c4790937c3eae3c5fcd247a48
Etienne BLANC <etienne@x-rator.com>
2022-12-07 17:54:35 +0100

    chore: add pnpm

--- apps/store/build.js
+++ apps/store/build.js
@@ -2,29 +2,20 @@
 
 const esbuild = require('esbuild')
 
+
 esbuild
   .build({
     logLevel: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
     entryPoints: ['src/app.js', 'src/prepare.js', 'src/import_demo.js'],
-    bundle: true,
     outdir: 'out',
+    bundle: true,
     // Reduce size in production:
     minify: process.env.NODE_ENV === 'production',
     platform: 'node',
     target: 'node16.16',
     external: [
-      // To solve a knex problem, mark as externals some libs
-      // TODO: remove when knex => Prisma migration is done
-      'sqlite3',
-      'better-sqlite3',
-      'mysql',
-      'mysql2',
-      'tedious',
-      'oracledb',
-      'pg-query-stream',
-      'oracledb',
-      'pg-native',
-    ],
+      './node_modules/*',
+    ]
   })
   .then((result) => console.log('Files built with success:', result))
   .catch(() => process.exit(1))
