c9f52ee0551dc1d6da3ad7ca4f8c8f9c62df1e2e
Etienne BLANC <etienne@x-rator.com>
2022-12-15 11:17:37 +0100

    feat: use first ip as main ip for all existing assets

--- apps/store/migrations/20221214162300_add_is_main_column_to_ip_table.js
+++ apps/store/migrations/20221214162300_add_is_main_column_to_ip_table.js
@@ -5,14 +5,22 @@
  * @param {import("knex").Knex} knex
  * @returns
  */
-exports.up = (knex) => {
+exports.up = async (knex) => {
   // If already setup, ignore
   if (knex.userParams.isSetup)
     return Promise.resolve()
 
-  return knex.schema.table('ip', (table) => {
+  // Add new "is_main" boolean column to "ip" table
+  await knex.schema.table('ip', (table) => {
     table.boolean('is_main').defaultTo(false)
   })
+
+  // Set "is_main" to true for the first IP of each existing asset server
+  await knex.raw(`
+    UPDATE ip 
+    SET is_main = TRUE 
+    WHERE id IN (SELECT MIN(id) AS id FROM ip GROUP BY asset_server_id)
+    `)
 }
 
 /**
