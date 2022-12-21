3e217e31fc3f4c991015ad0aa9e9a24054d3de99
Etienne BLANC <etienne@x-rator.com>
2022-12-21 12:18:47 +0100

    refactore: split login/forgat into != components

---
+++ apps/web/components/sign-in/Login.vue
@@ -0,0 +1,169 @@
+<script>
+// @ts-check
+// Libs
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+// Services
+import { loginService } from '~/services/auth'
+
+export default {
+  computed: {
+    ...mapGetters('user', ['isLoggedIn', 'wrongLogin']),
+  },
+  created() {
+    if (!this.isLoggedIn)
+      this.$store.dispatch('changePageTitle', this.$t('action.login'))
+    this.redirectIfLoggedIn()
+  },
+  emits: ['goToForgotPassword'],
+  layout: 'no-nav-bar',
+  data() {
+    return {
+      isLoading: false,
+      loginForm: {
+        password: '',
+        username: '',
+        valid: false,
+      },
+      rules: {
+        password: [v => !!v || 'Password is required'],
+        username: [v => !!v || 'Username or email required'],
+      },
+    }
+  },
+  methods: {
+    ...mapActions('user', ['authorize', 'updateLoginState']),
+    ...mapMutations(['CREATE_SESSION', 'WRONG_LOGIN']),
+    async login() {
+      try {
+        if (!this.$refs['login-form']?.validate())
+          return
+        this.isLoading = true
+        const { username, password } = this.loginForm
+        const { accessToken, user } = await loginService(this.$axios, {
+          password,
+          username,
+        })
+        /**
+                 * @return {void}
+                 */
+        this.authorize({
+          accessToken,
+          user: {
+            email: user.email,
+            firstName: user.firstName,
+            id: user.id,
+            lastName: user.lastName,
+            roles: user.roles,
+            username: user.username,
+          },
+        })
+        const redirectUrl = new URL(location.href).searchParams.get('redirect')
+        if (redirectUrl)
+          this.$router.push(decodeURIComponent(redirectUrl))
+        else
+          this.$router.push('/dashboard')
+      }
+      catch (error) {
+        console.error(error)
+      }
+      finally {
+        this.isLoading = false
+      }
+    },
+    redirectIfLoggedIn() {
+      const redirectUrl = new URL(location.href).searchParams.get('redirect')
+      if (redirectUrl) {
+        return this.$router.push(decodeURIComponent(redirectUrl))
+      }
+      else {
+        return this.$router.push(this.localePath({
+          name: 'dashboard',
+        }))
+      }
+    },
+    /**
+         * @return {void}
+         */
+    submitForm() {
+      this.login()
+    },
+  },
+  watch: {
+    /**
+         * @param {boolean} becomeConected - True if user is now connected
+         * @param {boolean} wasConnected - True if user was connected
+         */
+    isLoggedIn(becomeConected, _wasConnected) {
+      if (!becomeConected)
+        return
+      this.redirectIfLoggedIn()
+    },
+  },
+}
+</script>
+
+<template>
+  <v-card
+    style="position: relative"
+    class="mx-auto"
+    max-width="470"
+    color="card-bg"
+  >
+    <template #progress>
+      <v-progress-linear
+        style="position: absolute"
+        color="#1ab342"
+        height="5"
+        indeterminate
+      />
+    </template>
+    <v-card-title>
+      {{
+        $t('action.login')
+      }}
+    </v-card-title>
+
+    <v-card-text class="mt-5">
+      <v-form
+        ref="login-form"
+        v-model="loginForm.valid"
+        @keypress.enter="submit"
+        @submit.prevent="submitForm"
+      >
+        <v-text-field
+          v-model="loginForm.username"
+          v-test="'login-form-username'"
+          :rules="rules.username"
+          :label="$t('t.usernameOrEmail')"
+          autocomplete="username"
+          required
+        />
+        <v-text-field
+          v-model="loginForm.password"
+          v-test="'login-form-password'"
+          :rules="rules.password"
+          :label="$t('t.password')"
+          type="password"
+          autocomplete="current-password"
+          required
+        />
+        <div
+          class="blue--text text-right text-decoration-underline"
+          style="cursor: pointer"
+          @click="$emit('goToForgotPassword')"
+        >
+          {{ $t('t.forgotYourPassword') }}
+        </div>
+
+        <div class="text-right mt-4">
+          <v-btn
+            v-test="'login-form-login-btn'"
+            type="submit"
+          >
+            {{ $t('action.login') }}
+          </v-btn>
+        </div>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
---
+++ apps/web/components/sign-in/ResetPassword.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useContext } from '@nuxtjs/composition-api'
+import { sendResetPasswordMail } from '~/services/auth'
+
+const emit = defineEmits(['goToLogin'])
+
+const axios = useContext().$axios
+
+const isLoading = ref(false)
+const isFormValid = ref(false)
+const showFailMailSnackbar = ref(false)
+const showEmailSentSnackbar = ref(false)
+const username = ref('')
+
+// TODO use vee-validate instead
+const rules = {
+  password: [(v: string) => !!v || 'Password is required'],
+  username: [(v: string) => !!v || 'Username or email required'],
+}
+
+const resetPassword = async () => {
+  if (!isFormValid.value || !username.value)
+    return
+
+  try {
+    isLoading.value = true
+    const res = await sendResetPasswordMail(axios, {
+      username: username.value,
+    })
+    if (res.status === 201) {
+      showEmailSentSnackbar.value = true
+    }
+    else {
+      // it will be useful when we'll handle the error res. this.failMessage = this.$t('action.mailDoesntExist')
+    }
+  }
+  catch (error) {
+    showFailMailSnackbar.value = true
+    console.error(error)
+  }
+  isLoading.value = false
+}
+
+const handleGoToLoginClick = () => {
+  emit('goToLogin')
+}
+</script>
+
+<template>
+  <v-card
+    style="position: relative"
+    class="mx-auto"
+    max-width="470"
+    color="card-bg"
+  >
+    <template #progress>
+      <v-progress-linear
+        style="position: absolute"
+        color="#1ab342"
+        height="5"
+        indeterminate
+      />
+    </template>
+    <v-card-title>
+      Reset password
+    </v-card-title>
+
+    <v-card-text class="mt-5">
+      <v-form
+        ref="login-form"
+        v-model="isFormValid"
+        @keypress.enter="resetPassword"
+        @submit.prevent="resetPassword"
+      >
+        <v-text-field
+          v-model="username"
+          v-test="'reset-form-username'"
+          :rules="rules.username"
+          label="Username or email"
+          required
+        />
+
+        <div
+          class="blue--text text-left text-decoration-underline"
+          style="cursor: pointer"
+          @click="handleGoToLoginClick"
+        >
+          Return to login
+        </div>
+
+        <div class="text-right mt-4">
+          <v-btn
+            v-test="'login-form-reset-password-btn'"
+            :disabled="isLoading"
+            type="submit"
+          >
+            Reset password
+          </v-btn>
+        </div>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
