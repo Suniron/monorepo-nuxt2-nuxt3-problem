8dacedd65b66c45ce90dab24936025d36a5df7d1
Etienne BLANC <etienne@x-rator.com>
2023-01-11 17:41:12 +0100

    chore: increase test timeout to 5 minutes

--- __tests__/utils/setupTests.ts
+++ __tests__/utils/setupTests.ts
@@ -1,3 +1,12 @@
 // Load .env file
 import { config } from 'dotenv'
 config()
+
+/**
+ * Theese actions will be executed **before** each tests.
+ *
+ * It's used only when launch from vscode extension
+ */
+beforeAll(() => {
+  jest.setTimeout(60 * 5 * 1000)
+})
