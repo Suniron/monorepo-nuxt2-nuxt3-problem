28c316beac4680946e20ee3a3ffed46359a493d2
Etienne BLANC <etienne@x-rator.com>
2023-01-11 17:43:52 +0100

    refactor: clean

--- apps/store/types/express.d.ts
+++ apps/store/types/express.d.ts
@@ -3,26 +3,11 @@ import type { LoggedUser } from "../types/user";
 
 declare global {
   namespace Express {
-    type UserRole = 'admin' | 'member'
-
-    interface DefaultLoggedUser {
-      id: string
-      firstName: string
-      lastName: string
-      username: string
-      email: string
-      companyId: number
-      companyName: string
-      roles: UserRole[]
-      typ: string
-      iat: number
-      exp: number
-      aud: string
-      iss: string
-    }
-
     export interface Request {
-      user?: LoggedUser
+      /**
+       * In fact, user is not always defined but in most cases it is: only few routes not requiring auth don't have it.
+       */
+      user: LoggedUser
       log: Logger,
       duration?: number
     }
