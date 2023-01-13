eae7a5cb66b9e7c953979a2bce7c1c2e1cf5a96c
Etienne BLANC <etienne@x-rator.com>
2023-01-13 17:54:58 +0100

    refactor: clean

--- apps/store/__tests__/utils/setupTests.ts
+++ apps/store/__tests__/utils/setupTests.ts
@@ -1,27 +1,3 @@
 // Load .env file
 import { config } from 'dotenv'
 config()
-// import { TextEncoder } from 'util'
-
-// import { mockVerifyToken } from '../mocks'
-// import { generateUser } from '.'
-// global.TextEncoder = TextEncoder
-
-// const customUser = generateUser({
-//   firstName: 'Etienne-test',
-//   lastName: 'Blanc-test',
-// })
-/**
- * Theese actions will be executed **before** each tests
- */
-beforeAll(() => {
-  // jest.setTimeout(30000)
-  // mockVerifyToken(customUser)
-})
-
-/**
- * Theese actions will be executed **before** each tests
- */
-afterAll(() => {
-  // TODO
-})
